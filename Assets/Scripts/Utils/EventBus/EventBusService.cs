8f5355a1e9f5d279260c1538fa1fa755f6d3d75b
Kinderman de Freitas Barros <kindbarros@gmail.com>
2025-06-01 11:35:08 -0400

    WON: Fix EventBus implementation

--- Assets/Scripts/Utils/EventBus/EventBusService.cs
+++ Assets/Scripts/Utils/EventBus/EventBusService.cs
@@ -5,31 +5,31 @@ namespace WeatherOrNot.Utils
 {
     public class EventBusService
     {
-        private readonly Dictionary<Type, List<Delegate>> _subscribers = new();
+        private readonly Dictionary<Type, List<Delegate>> m_subscribers = new();
 
         public void Subscribe<T>(Action<T> callback)
         {
             var type = typeof(T);
-            if (!_subscribers.ContainsKey(type))
-                _subscribers[type] = new List<Delegate>();
+            if (!m_subscribers.ContainsKey(type))
+                m_subscribers[type] = new List<Delegate>();
 
-            _subscribers[type].Add(callback);
+            m_subscribers[type].Add(callback);
         }
 
         public void Unsubscribe<T>(Action<T> callback)
         {
             var type = typeof(T);
-            if (!_subscribers.ContainsKey(type)) return;
+            if (!m_subscribers.ContainsKey(type)) return;
 
-            _subscribers[type].Remove(callback);
-            if (_subscribers[type].Count == 0)
-                _subscribers.Remove(type);
+            m_subscribers[type].Remove(callback);
+            if (m_subscribers[type].Count == 0)
+                m_subscribers.Remove(type);
         }
 
         public void Notify<T>(object sender, T evt)
         {
             var type = typeof(T);
-            if (!_subscribers.TryGetValue(type, out var callbacks)) return;
+            if (!m_subscribers.TryGetValue(type, out var callbacks)) return;
 
             foreach (var callback in callbacks)
                 ((Action<T>)callback).Invoke(evt);

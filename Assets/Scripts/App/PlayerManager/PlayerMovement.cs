701e4abc6392e85e6efaff003c8209f98e237b07
Kinderman de Freitas Barros <kindbarros@gmail.com>
2025-06-05 00:22:52 -0400

    WON: Improved player movement

--- Assets/Scripts/App/PlayerManager/PlayerMovement.cs
+++ Assets/Scripts/App/PlayerManager/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using WeatherOrNot.Events.Animation;
 using WeatherOrNot.Events.Weather;
@@ -35,24 +36,23 @@ namespace WeatherOrNot.App.PlayerManager
         [SerializeField] private float m_dashDuration = 0.2f;
         [SerializeField] private float m_dashCooldown = 1f;
 
-
         [Header("Player SFX")] [SerializeField]
         private AudioSource m_audioSource;
+
         [SerializeField] private AudioClip m_jumpSound;
         [SerializeField] private AudioClip m_dashSound;
         [SerializeField] private AudioClip m_landSound;
         [SerializeField] private AudioClip m_footstepsSound1;
         [SerializeField] private AudioClip m_footstepsSound2;
 
-
         [Header("Weather SFX")] [SerializeField]
         private AudioSource weatherAudioSource;
+
         [SerializeField] private AudioClip rainSound;
         [SerializeField] private AudioClip snowSound;
         [SerializeField] private AudioClip sunnySound;
         [SerializeField] private AudioClip thunderstormSound;
         [SerializeField] private AudioClip windySound;
-
 
         private Rigidbody2D m_rb;
         private float m_horizontalInput;
@@ -73,14 +73,18 @@ namespace WeatherOrNot.App.PlayerManager
         private float m_footstepInterval = 0.4f;
         private int m_footstepIndex = 0;
         private bool m_canPlayFootstep = true;
-        private bool isMoving => Mathf.Abs(m_horizontalInput) > 0.1f;
+        private bool IsMoving => Mathf.Abs(m_horizontalInput) > 0.1f;
+
+        private float m_wallJumpLockTime = 0.10f;
+        private float m_lastWallJumpTime = -Mathf.Infinity;
+        private float m_groundLockTime = 0.08f;
+        private float m_lastLeftGroundTime = -Mathf.Infinity;
 
         private void Awake()
         {
             m_rb = GetComponent<Rigidbody2D>();
             m_lastGroundedTime = Time.time;
             m_rb.linearVelocity = Vector2.zero;
-            weatherAudioSource = Camera.main.GetComponent<AudioSource>();
         }
 
         private void Update()
@@ -88,28 +92,12 @@ namespace WeatherOrNot.App.PlayerManager
             if (Time.time < 0.1f) return;
 
             HandleInput();
-            m_horizontalInput = Input.GetAxisRaw("Horizontal");
-
-            if (Input.GetButtonDown("Jump"))
-                m_lastJumpPressedTime = Time.time;
-
-            if (Input.GetKeyDown(KeyCode.Z))
-                EventBus.Notify(this, new ChangeWeatherEvent(WeatherTypes.Snow));
-            if (Input.GetKeyDown(KeyCode.X))
-                EventBus.Notify(this, new ChangeWeatherEvent(WeatherTypes.Clear));
-            if (Input.GetKeyDown(KeyCode.C))
-                EventBus.Notify(this, new ChangeWeatherEvent(WeatherTypes.Thunderstorm));
-            if (Input.GetKeyDown(KeyCode.V))
-                EventBus.Notify(this, new ChangeWeatherEvent(WeatherTypes.Windy));
-            if (Input.GetKeyDown(KeyCode.B))
-                EventBus.Notify(this, new ChangeWeatherEvent(WeatherTypes.Rain));
-
             FlipCharacter(m_horizontalInput);
             HandleWallSlide();
             HandleJump();
             HandleAnimations();
 
-            if (isMoving && m_canPlayFootstep)
+            if (IsMoving && m_canPlayFootstep)
             {
                 StartCoroutine(PlayFootstepWithDelay());
             }
@@ -154,11 +142,17 @@ namespace WeatherOrNot.App.PlayerManager
             if (Input.GetKeyDown(KeyCode.Q))
                 TryDash();
 
-            if (Input.GetKeyDown(KeyCode.Z)) SetSnowy();
-            if (Input.GetKeyDown(KeyCode.X)) SetSunny();
-            if (Input.GetKeyDown(KeyCode.C)) SetThunderstorm();
-            if (Input.GetKeyDown(KeyCode.V)) SetWindy();
-            if (Input.GetKeyDown(KeyCode.B)) SetRain();
+            if (Input.GetKeyDown(KeyCode.Z)) SetWeather(WeatherTypes.Snow, snowSound);
+            if (Input.GetKeyDown(KeyCode.X)) SetWeather(WeatherTypes.Clear, sunnySound);
+            if (Input.GetKeyDown(KeyCode.C)) SetWeather(WeatherTypes.Thunderstorm, thunderstormSound);
+            if (Input.GetKeyDown(KeyCode.V)) SetWeather(WeatherTypes.Windy, windySound);
+            if (Input.GetKeyDown(KeyCode.B)) SetWeather(WeatherTypes.Rain, rainSound);
+        }
+
+        private void SetWeather(WeatherTypes weatherType, AudioClip sound)
+        {
+            TryChangeWeather(weatherType);
+            PlayWeatherSound(sound);
         }
 
         private void ApplyMovement()
@@ -184,72 +178,111 @@ namespace WeatherOrNot.App.PlayerManager
             if (Time.time > m_dashTime)
             {
                 m_isDashing = false;
-                //TODO: End Dash Animation
             }
         }
 
         private IEnumerator PlayFootstepWithDelay()
         {
             m_canPlayFootstep = false;
-            if (m_isGrounded && isMoving)
+            if (m_isGrounded && IsMoving)
             {
                 AudioClip clipToPlay = m_footstepIndex == 0 ? m_footstepsSound1 : m_footstepsSound2;
                 if (clipToPlay != null)
                 {
                     m_audioSource.PlayOneShot(clipToPlay);
                 }
+
                 m_footstepIndex = 1 - m_footstepIndex;
             }
+
             yield return new WaitForSeconds(m_footstepInterval);
             m_canPlayFootstep = true;
         }
 
         private void HandleWallSlide()
         {
+            bool wasWallSliding = m_isWallSliding;
             m_isWallSliding = m_isTouchingWall && !m_isGrounded && m_rb.linearVelocity.y < 0f;
-            if (m_isWallSliding)
+
+            if (m_isWallSliding && !wasWallSliding)
             {
                 EventBus.Notify(this, new StartWallSlidingEvent());
 
+                m_lastDashTime = -Mathf.Infinity;
             }
         }
 
         private void HandleJump()
         {
             var jumpBuffered = Time.time - m_lastJumpPressedTime <= m_jumpBufferTime;
-            var canUseCoyote = Time.time - m_lastGroundedTime <= m_coyoteTime;
-
             if (!jumpBuffered) return;
 
-            bool playedJumpSound = false;
+            var wallJumpLock = Time.time - m_lastWallJumpTime <= m_wallJumpLockTime;
 
-            if (m_isWallSliding)
+            if (m_isWallSliding && !wallJumpLock)
             {
-                var force = new Vector2(-m_wallDirection * m_wallJumpDirection.x * m_wallJumpForce,
-                    m_wallJumpDirection.y * m_wallJumpForce);
-                m_rb.linearVelocity = Vector2.zero;
-                m_rb.AddForce(force, ForceMode2D.Impulse);
-                m_lastJumpPressedTime = -1;
-                
-                EventBus.Notify(this, new StartWallJumpingEvent());
-                playedJumpSound = true;
-
-                //TODO: Play Wall Jump Animation
+                ExecuteWallJump();
+                return;
             }
-            else if (m_isGrounded || canUseCoyote)
-            {
-                m_rb.linearVelocity = new Vector2(m_rb.linearVelocity.x, m_jumpUpwardSpeed);
-                m_lastJumpPressedTime = -1;
-                EventBus.Notify(this, new StartJumpingEvent());
-                playedJumpSound = true;
 
-                if (playedJumpSound && m_jumpSound != null)
+            var canUseCoyote = Time.time - m_lastGroundedTime <= m_coyoteTime;
+            if ((m_isGrounded || canUseCoyote) && !wallJumpLock)
+            {
+                ExecuteNormalJump();
+            }
+        }
+
+        private void ExecuteWallJump()
+        {
+            var force = new Vector2(-m_wallDirection * m_wallJumpDirection.x * m_wallJumpForce,
+                m_wallJumpDirection.y * m_wallJumpForce);
+
+            m_rb.linearVelocity = Vector2.zero;
+            m_rb.AddForce(force, ForceMode2D.Impulse);
+
+            m_lastJumpPressedTime = -1;
+            m_lastWallJumpTime = Time.time;
+            m_lastLeftGroundTime = Time.time;
+
+            m_isWallSliding = false;
+            m_isTouchingWall = false;
+            m_isGrounded = false;
+
+            var originalFacing = m_isFacingRight;
+            if ((m_wallDirection == -1 && !m_isFacingRight) || (m_wallDirection == 1 && m_isFacingRight))
+            {
+                FlipCharacter(m_isFacingRight ? -1 : 1);
+            }
+
+            RestoreOriginalFacing(originalFacing).Forget();
+
+            EventBus.Notify(this, new StartWallJumpingEvent());
+            if (m_jumpSound != null) m_audioSource.PlayOneShot(m_jumpSound);
+        }
+
+        private async UniTask RestoreOriginalFacing(bool originalFacing)
+        {
+            await UniTask.Delay(1000);
+
+            if (!m_isGrounded)
+            {
+                if (originalFacing != m_isFacingRight)
                 {
-                    m_audioSource.PlayOneShot(m_jumpSound);
+                    var scale = transform.localScale;
+                    scale.x *= -1;
+                    transform.localScale = scale;
+                    m_isFacingRight = originalFacing;
                 }
-
-                //TODO: Play Jump Animation
             }
+        }
+
+        private void ExecuteNormalJump()
+        {
+            m_rb.linearVelocity = new Vector2(m_rb.linearVelocity.x, m_jumpUpwardSpeed);
+            m_lastJumpPressedTime = -1;
+
+            EventBus.Notify(this, new StartJumpingEvent());
+            if (m_jumpSound != null) m_audioSource.PlayOneShot(m_jumpSound);
         }
 
         private void HandleAnimations()
@@ -260,15 +293,11 @@ namespace WeatherOrNot.App.PlayerManager
             {
                 if (m_rb.linearVelocity.y > 0)
                 {
-                    //TODO: Play Jump Rising Animation
                     EventBus.Notify(this, new StartJumpingEvent());
-
                 }
                 else if (m_rb.linearVelocity.y < 0)
                 {
-                    //TODO: Play Falling Animation
                     EventBus.Notify(this, new StartEndJumpingEvent());
-
                 }
 
                 return;
@@ -280,12 +309,8 @@ namespace WeatherOrNot.App.PlayerManager
             }
             else
             {
-                //TODO: Play Idle Animation
                 EventBus.Notify(this, new StartIdleEvent());
-                return;
             }
-
-            //EventBus.Notify(this, new ChangeWeatherEvent(weather));
         }
 
         private void PlayWeatherSound(AudioClip clip)
@@ -297,36 +322,6 @@ namespace WeatherOrNot.App.PlayerManager
             weatherAudioSource.Play();
         }
 
-        private void SetRain()
-        {
-            TryChangeWeather(WeatherTypes.Rain);
-            PlayWeatherSound(rainSound);
-        }
-
-        private void SetSnowy()
-        {
-            TryChangeWeather(WeatherTypes.Snow);
-            PlayWeatherSound(snowSound);
-        }
-
-        private void SetSunny()
-        {
-            TryChangeWeather(WeatherTypes.Clear);
-            PlayWeatherSound(sunnySound);
-        }
-        
-        private void SetThunderstorm()
-        {
-            TryChangeWeather(WeatherTypes.Thunderstorm);
-            PlayWeatherSound(thunderstormSound);
-        }
-
-        private void SetWindy()
-        {
-            TryChangeWeather(WeatherTypes.Windy);
-            PlayWeatherSound(windySound);
-        }
-
         private void FlipCharacter(float direction)
         {
             switch (direction)
@@ -335,19 +330,21 @@ namespace WeatherOrNot.App.PlayerManager
                     return;
                 case > 0 when !m_isFacingRight:
                 case < 0 when m_isFacingRight:
-                    {
-                        m_isFacingRight = !m_isFacingRight;
-                        var scale = transform.localScale;
-                        scale.x *= -1;
-                        transform.localScale = scale;
-                        break;
-                    }
+                {
+                    m_isFacingRight = !m_isFacingRight;
+                    var scale = transform.localScale;
+                    scale.x *= -1;
+                    transform.localScale = scale;
+                    break;
+                }
             }
         }
 
         private void TryDash()
         {
             if (Time.time < m_lastDashTime + m_dashCooldown) return;
+
+            if (!m_isGrounded && m_lastDashTime > m_lastGroundedTime) return;
 
             m_isDashing = true;
             m_dashTime = Time.time + m_dashDuration;
@@ -364,34 +361,108 @@ namespace WeatherOrNot.App.PlayerManager
             EventBus.Notify(this, new ChangeWeatherEvent(weather));
         }
 
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            ProcessCollisionContacts(collision);
+        }
+
         private void OnCollisionStay2D(Collision2D collision)
         {
-            m_isGrounded = false;
-            m_isTouchingWall = false;
+            ProcessCollisionContacts(collision);
+        }
 
-            foreach (var contact in collision.contacts)
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            var stillGrounded = false;
+            var stillTouchingWall = false;
+
+            if (collision.contactCount > 0)
             {
-                if (contact.normal.y > 0.5f)
+                foreach (var contact in collision.contacts)
                 {
-                    m_isGrounded = true;
-                    m_lastGroundedTime = Time.time;
-                    if (!m_isGrounded && contact.normal.y > 0.5f && m_landSound != null)
+                    if (contact.normal.y > 0.5f)
                     {
-                        m_audioSource.PlayOneShot(m_landSound);
+                        stillGrounded = true;
+                    }
+                    else if (Mathf.Abs(contact.normal.x) > 0.5f)
+                    {
+                        stillTouchingWall = true;
+                        m_wallDirection = contact.normal.x > 0 ? -1 : 1;
                     }
                 }
-                else if (Mathf.Abs(contact.normal.x) > 0.5f)
+            }
+
+            if (!stillGrounded || !stillTouchingWall)
+            {
+                var contacts = new Collider2D[10];
+                var filter = new ContactFilter2D().NoFilter();
+                filter.useTriggers = false;
+
+                int numContacts = GetComponent<Collider2D>().Overlap(filter, contacts);
+
+                for (int i = 0; i < numContacts; i++)
                 {
-                    m_isTouchingWall = true;
-                    m_wallDirection = contact.normal.x > 0 ? -1 : 1;
+                    if (contacts[i] == collision.collider) continue;
+
+                    Vector2 direction = (contacts[i].transform.position - transform.position).normalized;
+
+                    if (!stillGrounded && Vector2.Dot(Vector2.up, direction) < -0.5f)
+                    {
+                        stillGrounded = true;
+                    }
+                    else if (!stillTouchingWall && Mathf.Abs(Vector2.Dot(Vector2.right, direction)) > 0.5f)
+                    {
+                        stillTouchingWall = true;
+                        m_wallDirection = Vector2.Dot(Vector2.right, direction) > 0 ? -1 : 1;
+                    }
+
+                    if (stillGrounded && stillTouchingWall) break;
                 }
+            }
+
+            if (!stillGrounded)
+            {
+                m_isGrounded = false;
+                m_lastLeftGroundTime = Time.time;
+            }
+
+            if (!stillTouchingWall)
+            {
+                m_isTouchingWall = false;
             }
         }
 
-        private void OnCollisionExit2D()
+        private void ProcessCollisionContacts(Collision2D collision)
         {
-            m_isGrounded = false;
-            m_isTouchingWall = false;
+            if (collision.contactCount <= 0) return;
+
+            bool foundGround = false;
+            bool foundWall = false;
+
+            foreach (var contact in collision.contacts)
+            {
+                if (!foundGround && contact.normal.y > 0.5f)
+                {
+                    if (Time.time - m_lastWallJumpTime > m_groundLockTime)
+                    {
+                        foundGround = true;
+
+                        if (!m_isGrounded && m_landSound != null && m_rb.linearVelocity.y <= 0)
+                            m_audioSource.PlayOneShot(m_landSound);
+
+                        m_isGrounded = true;
+                        m_lastGroundedTime = Time.time;
+                    }
+                }
+                else if (!foundWall && Mathf.Abs(contact.normal.x) > 0.5f)
+                {
+                    foundWall = true;
+                    m_isTouchingWall = true;
+                    m_wallDirection = contact.normal.x > 0 ? -1 : 1;
+                }
+
+                if (foundGround && foundWall) break;
+            }
         }
     }
 }
